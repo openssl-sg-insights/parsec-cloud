3e701be730207655dcc410d443ea4c8d9a23b99d
firelight flagboy <firelight.flagboy@gmail.com>
2022-09-23 11:21:26 +0200

    Add test to check for leak of file descriptor

--- tests/core/fs/test_fs_online_rwfile_and_sync.py
+++ tests/core/fs/test_fs_online_rwfile_and_sync.py
@@ -1,6 +1,11 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 
+from typing import List, TypedDict
 import pytest
+import trio
+import psutil
+
+# import gc
 import structlog
 from hypothesis import strategies as st
 from hypothesis_trio.stateful import initialize, rule
@@ -13,6 +18,128 @@ from tests.common import FileOracle
 
 BLOCK_SIZE = 16
 PLAYGROUND_SIZE = BLOCK_SIZE * 10
+
+
+@pytest.mark.trio
+@pytest.mark.slow
+async def test_leak_opened_file(user_fs_online_state_machine, alice: LocalDevice):
+    class FSOnlineRwFileAndAsync(user_fs_online_state_machine):
+        async def init(self):
+            await self.reset_all()
+            await self.start_backend()
+            self.device = self.correct_addr(alice)
+            await self.restart_user_fs(self.device)
+            self.wid = await self.user_fs.workspace_create(EntryName("w"))
+
+        async def restart(self):
+            await self.restart_user_fs(self.device)
+
+        def get_root_nursery(self):
+            return nursery
+
+        @rule()
+        async def ignore(self):
+            pass
+
+    proc = psutil.Process()
+
+    class ProcFdStat(TypedDict):
+        num_fds: int
+        file_opened_count: int
+        file_opened: List["psutil.popenfile"]
+        net_connections_count: int
+        net_connection: List["psutil.pconn"]
+
+    def get_proc_fd_stats() -> ProcFdStat:
+        num_fds = proc.num_fds()
+        file_opened = proc.open_files()
+        net_conn = proc.connections(kind="all")
+
+        return {
+            "num_fds": num_fds,
+            "file_opened_count": len(file_opened),
+            "file_opened": file_opened,
+            "net_connections_count": len(net_conn),
+            "net_connections": net_conn,
+        }
+
+    def present_proc_fd_stat(stat: ProcFdStat) -> str:
+        return f"num_fds={stat['num_fds']}, file_opened={stat['file_opened_count']}, net_connections={stat['net_connections_count']}"
+
+    class WatchFileOpened:
+        def __init__(self, fuse_count: int) -> None:
+            self.fuse_count = int(fuse_count)
+            self.fuse = 0
+            self.update_file_opened()
+            self.previous_file_opened = self.file_opened
+
+        def update_file_opened(self):
+            self.stat = get_proc_fd_stats()
+            self.file_opened = self.stat["file_opened"]
+
+        def check_file_opened(self):
+            self.update_file_opened()
+            if len(self.previous_file_opened) < len(self.file_opened):
+                self.break_fuse()
+            self.previous_file_opened = self.file_opened
+
+        def break_fuse(self):
+            print("A fuse has break")
+            self.fuse += 1
+            if self.fuse > self.fuse_count:
+                raise ValueError(
+                    "Break all available fuse, because of a leaks of opened file. "
+                    "Increasing opened file:\n{}".format(
+                        "\n".join(map(lambda file: str(file), self.file_opened))
+                    )
+                )
+
+        def reset_fuse(self):
+            print("Resetting the fuses")
+            self.fuse = 0
+
+    NUMBER_LOOP = NUMBER_RESTART_LOOP = 10
+    watch_file_opened = WatchFileOpened(NUMBER_RESTART_LOOP * 0.75)
+
+    # The number of fuse to break before failing
+    async with trio.open_nursery() as nursery:
+        instance = FSOnlineRwFileAndAsync()
+        for i in range(NUMBER_LOOP):
+            watch_file_opened.reset_fuse()
+            print(f"Loop#{i:02} Initialization")
+            await instance.init()
+
+            for j in range(NUMBER_RESTART_LOOP):
+                watch_file_opened.check_file_opened()
+
+                print(
+                    "Loop#{:02}-#{:02} Restart {}".format(
+                        i,
+                        j,
+                        present_proc_fd_stat(watch_file_opened.stat),
+                    )
+                )
+                await instance.restart()
+                watch_file_opened.check_file_opened()
+
+                print(
+                    "Loop#{:02}-#{:02} After Restart {}".format(
+                        i,
+                        j,
+                        present_proc_fd_stat(watch_file_opened.stat),
+                    )
+                )
+                # unreachable = gc.collect()
+                # watch_file_opened.update_file_opened()
+                # print(
+                #     "Loop#{:02}-#{:02} After Garbage {}, gc_unreachable={}".format(
+                #         i,
+                #         j,
+                #         present_proc_fd_stat(watch_file_opened.stat),
+                #         unreachable,
+                #     )
+                # )
+        nursery.cancel_scope.cancel()
 
 
 @pytest.fixture
